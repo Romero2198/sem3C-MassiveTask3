72e1aaa7aba82dfa390ba5298c9f3f107199e87a
Yung.Energy <morskojroma@gmail.com>
2022-07-02 19:39:11 +0300

    Task3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int n = InputInt("Введите число: ");
+int count = 1;
+while (count <= n)
+{
+    int cube = count * count * count;
+    Console.Write(cube + ", ");
+    count++;
+}
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
